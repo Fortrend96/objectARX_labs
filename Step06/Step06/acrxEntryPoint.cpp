84369029f1b8c7d6dcd7fcd11ad1d92373311294
fortrend <serbin-96@mail.ru>
2024-08-21 15:22:59 +0700

    Добаавлены правки в лабу 6

--- Step06/Step06/acrxEntryPoint.cpp
+++ Step06/Step06/acrxEntryPoint.cpp
@@ -33,6 +33,35 @@
 //----- ObjectARX EntryPoint
 class CStep06App : public AcRxArxApp {
 
+private:
+	// функция получения таблицы блоков
+	static AcDbBlockTable* GetPBlockTable()
+	{
+		AcDbBlockTable* pBlockTable = nullptr; // таблица блоков
+
+		AcDbHostApplicationServices* pHostAppServices = acdbHostApplicationServices();
+		if (!pHostAppServices) {
+			acutPrintf(L"\nНе удалось получить объект HostAppServices!");
+			return pBlockTable;
+		}
+
+
+		AcDbDatabase* pWorkingDatabase = pHostAppServices->workingDatabase();
+		if (!pWorkingDatabase) {
+			acutPrintf(L"\nНе удалось получить объект WorkingDatabase!");
+			return pBlockTable;
+		}
+
+		// получаем таблицу блоков в режиме чтения
+		if (pWorkingDatabase->getBlockTable(pBlockTable, AcDb::kForRead) != Acad::eOk) {
+			acutPrintf(L"\nНе удалось открыть таблицу блоков!");
+			return pBlockTable;
+		}
+
+		return pBlockTable;
+	}
+
+
 public:
 	CStep06App () : AcRxArxApp () {}
 
@@ -63,8 +92,7 @@ public:
 
 	static void AsdkStep06_CREATEEMPLOYEE(void)
 	{
-		/*
-		
+		/*	
 		1. Введите данные о сотруднике для идентификатора сотрудника: 
 			кабинки сотрудника; 
 			имени сотрудника; 
@@ -89,49 +117,35 @@ public:
 			|| acedGetInt(L"Введите номер кабинки: ", &cubeNumber) != RTNORM
 			|| acedGetString(0, L"Введите имя сотрудника: ", strFirstName) != RTNORM
 			|| acedGetString(0, L"Введите фамилию сотрудника: ", strLastName) != RTNORM
-			|| acedGetPoint(NULL, L"Введите должность сотрудника: ", asDblArray(pt)) != RTNORM
-			) 
+			|| acedGetPoint(NULL, L"Введите должность сотрудника: ", asDblArray(pt)) != RTNORM) 
 			return;
 		
 
 		// получаем указатель на текущий чертеж и указатель на таблицу блоков чертежа. Открыть ее для чтения.
-		AcDbBlockTable* pBlockTable; // таблица блоков
-		if (acdbHostApplicationServices()->workingDatabase()->getBlockTable(pBlockTable, AcDb::kForRead) == Acad::eOk) 
+		AcDbBlockTable* pBlockTable = GetPBlockTable(); // таблица блоков
+		if(pBlockTable)
 		{
 			AcDbBlockTableRecord* pSpaceRecord; // запись пространства модели
 			if (pBlockTable->getAt(ACDB_MODEL_SPACE, pSpaceRecord, AcDb::kForWrite) == Acad::eOk) 
 			{
-				AsdkEmployee* pemployeeEntity = new AsdkEmployee;
-				pemployeeEntity->setID(id);
-				pemployeeEntity->setCube(cubeNumber);
-				pemployeeEntity->setFirstName(strFirstName);
-				pemployeeEntity->setLastName(strLastName);
-				pemployeeEntity->setCenter(pt);
+				AcDbObjectPointer<AsdkEmployee> pEmployeeEntity;
+				pEmployeeEntity.create();
+				pEmployeeEntity->setID(id);
+				pEmployeeEntity->setCube(cubeNumber);
+				pEmployeeEntity->setFirstName(strFirstName);
+				pEmployeeEntity->setLastName(strLastName);
+				pEmployeeEntity->setCenter(pt);
 				// добавляем сущность в пространство модели
 				AcDbObjectId idObj;
-				if (pSpaceRecord->appendAcDbEntity(idObj, pemployeeEntity) == Acad::eOk)
-					pemployeeEntity->close();
-				else
-					delete pemployeeEntity;
+				if (pSpaceRecord->appendAcDbEntity(idObj, pEmployeeEntity) == Acad::eOk)
+					pEmployeeEntity->close();
+				
 				pSpaceRecord->close();
 			}
 			pBlockTable->close();
 		}
 	}
 	
-	// The ACED_ARXCOMMAND_ENTRY_AUTO macro can be applied to any static member 
-	// function of the CStep06App class.
-	// The function should take no arguments and return nothing.
-	//
-	// NOTE: ACED_ARXCOMMAND_ENTRY_AUTO has overloads where you can provide resourceid and
-	// have arguments to define context and command mechanism.
-	
-	// ACED_ARXCOMMAND_ENTRY_AUTO(classname, group, globCmd, locCmd, cmdFlags, UIContext)
-	// ACED_ARXCOMMAND_ENTRYBYID_AUTO(classname, group, globCmd, locCmdId, cmdFlags, UIContext)
-	// only differs that it creates a localized name using a string in the resource file
-	//   locCmdId - resource ID for localized command
-
-	// Modal Command with localized name
 	// ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL)
 	static void ASMyGroupMyCommand () {
 		// Put your command code here
@@ -143,50 +157,18 @@ public:
 	static void ASMyGroupMyPickFirst () {
 		ads_name result ;
 		int iRet =acedSSGet (ACRX_T("_I"), NULL, NULL, NULL, result) ;
-		if ( iRet == RTNORM )
-		{
-			// There are selected entities
-			// Put your command using pickfirst set code here
-		}
-		else
-		{
-			// There are no selected entities
-			// Put your command code here
-		}
+		if (iRet == RTNORM) {}
+		else {}
+		
 	}
 
-	// Application Session Command with localized name
 	// ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MySessionCmd, MySessionCmdLocal, ACRX_CMD_MODAL | ACRX_CMD_SESSION)
 	static void ASMyGroupMySessionCmd () {
 		// Put your command code here
 	}
 
-	// The ACED_ADSFUNCTION_ENTRY_AUTO / ACED_ADSCOMMAND_ENTRY_AUTO macros can be applied to any static member 
-	// function of the CStep06App class.
-	// The function may or may not take arguments and have to return RTNORM, RTERROR, RTCAN, RTFAIL, RTREJ to AutoCAD, but use
-	// acedRetNil, acedRetT, acedRetVoid, acedRetInt, acedRetReal, acedRetStr, acedRetPoint, acedRetName, acedRetList, acedRetVal to return
-	// a value to the Lisp interpreter.
-	//
-	// NOTE: ACED_ADSFUNCTION_ENTRY_AUTO / ACED_ADSCOMMAND_ENTRY_AUTO has overloads where you can provide resourceid.
-	
-	//- ACED_ADSFUNCTION_ENTRY_AUTO(classname, name, regFunc) - this example
-	//- ACED_ADSSYMBOL_ENTRYBYID_AUTO(classname, name, nameId, regFunc) - only differs that it creates a localized name using a string in the resource file
-	//- ACED_ADSCOMMAND_ENTRY_AUTO(classname, name, regFunc) - a Lisp command (prefix C:)
-	//- ACED_ADSCOMMAND_ENTRYBYID_AUTO(classname, name, nameId, regFunc) - only differs that it creates a localized name using a string in the resource file
-
-	// Lisp Function is similar to ARX Command but it creates a lisp 
-	// callable function. Many return types are supported not just string
-	// or integer.
 	// ACED_ADSFUNCTION_ENTRY_AUTO(CStep06App, MyLispFunction, false)
 	static int ads_MyLispFunction () {
-		//struct resbuf *args =acedGetArgs () ;
-		
-		// Put your command code here
-
-		//acutRelRb (args) ;
-		
-		// Return a value to the AutoCAD Lisp Interpreter
-		// acedRetNil, acedRetT, acedRetVoid, acedRetInt, acedRetReal, acedRetStr, acedRetPoint, acedRetName, acedRetList, acedRetVal
 
 		return (RTNORM) ;
 	}
@@ -196,9 +178,9 @@ public:
 //-----------------------------------------------------------------------------
 IMPLEMENT_ARX_ENTRYPOINT(CStep06App)
 
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL, NULL)
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MyPickFirst, MyPickFirstLocal, ACRX_CMD_MODAL | ACRX_CMD_USEPICKSET, NULL)
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MySessionCmd, MySessionCmdLocal, ACRX_CMD_MODAL | ACRX_CMD_SESSION, NULL)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL, nullptr)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MyPickFirst, MyPickFirstLocal, ACRX_CMD_MODAL | ACRX_CMD_USEPICKSET, nullptr)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, ASMyGroup, MySessionCmd, MySessionCmdLocal, ACRX_CMD_MODAL | ACRX_CMD_SESSION, nullptr)
 ACED_ADSSYMBOL_ENTRY_AUTO(CStep06App, MyLispFunction, false)
 
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, AsdkStep06, _CREATEEMPLOYEE, CREATEEMPLOYEE, ACRX_CMD_TRANSPARENT, NULL)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep06App, AsdkStep06, _CREATEEMPLOYEE, CREATEEMPLOYEE, ACRX_CMD_TRANSPARENT, nullptr)
